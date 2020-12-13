81a28e528dbcca3b75297425967810c99b4d788b
robaElshazly <roba1818@gmail.com>
2020-12-13 19:55:17 +1000

    add shell file run.sh and code to run the project

---
+++ src/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#install the necessary gems if you don't have them
+gem install bundler
+bundle install
+#clear the screen to start the app cleaned
+clear
+# run the app with either 2 parameters or no parameters
+if [ $# -eq 2 ]
+then
+ ruby app.rb $1 $2
+else 
+ ruby app.rb
+fi
